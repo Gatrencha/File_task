3054251f15e3ad958a8465833afc88fe48299c2a
Gatrencha <nice.vurkhiz@gmail.com>
2022-09-17 22:44:20 +0300

    Исправлена ошибка. Cook_book был не словарем а списком

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 
-cook_book = []
-
+cook_book = {}
 try:
     with open('recipes.txt', 'r', encoding='utf-8') as f:
         for line in f:
             dish_name = line.rstrip('\n')
-            recipe = {dish_name:[]}
+            recipe = []
             dish_count = f.readline()
             for i in range(int(dish_count)):
                 prod_all = f.readline()
                 prod_name, prod_count, prod_ue = prod_all.split(' | ')
                 prod_ever = {'ingredient_name': prod_name, 'quantity': prod_count, 'measure': prod_ue.rstrip('\n')}
-                recipe[dish_name].append(prod_ever)
+                recipe.append(prod_ever)
             f.readline()
-            cook_book.append(recipe)
-
+            cook_book[dish_name] = recipe
     print(cook_book)
 
 except FileNotFoundError:
@@ -22,14 +20,14 @@ except FileNotFoundError:
 
 
 
-#def get_shop_list_by_dishes(person, book):
-#    for recipes in book:
-#        print(recipes[0].title(), ":")
-#        for ingredient in recipes[1]:
-#            print(f"{ingredient[0]} {ingredient[1]*person} {ingredient[2]}")
-#        print("\n")
+def get_shop_list_by_dishes(person, book):
+    for recipes,products in book.items():
+        print(recipes, ":")
+        for products in book[recipes]:
+            print(f"{products['ingredient_name']} {products['quantity']*person} {products['measure']}")
+        print("\n")
 
 
-
-#person_count = input('Введите количество персон: ')
+person_count = input('Введите количество персон: ')
+get_shop_list_by_dishes(person_count,cook_book)
 
